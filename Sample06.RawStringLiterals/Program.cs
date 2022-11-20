557db7ea692505100bfda7a7dbabc85b8f55c81a
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-11-20 13:19:50 +0100

    Add RawStringLiterals and ExtendedNameofScope

---
+++ Sample06.RawStringLiterals/Program.cs
@@ -0,0 +1,67 @@
+﻿const string pi = "3.14";
+
+const string msg1 = """
+1. Raw
+	String
+		Literals
+""";
+
+const string msg2 = """
+					2. Raw
+						String
+							Literals
+					""";
+
+const string msg3 = """
+3. Raw
+	String
+		"Literals"
+""";
+	
+const string msg4 = """
+4. Raw
+	String
+		\"Literals\"
+""";
+
+const string msg5 = $"""
+5. Raw
+	String {pi}
+		\"Literals\"
+""";
+
+const string msg6 = $$"""
+6. Raw
+	String {{pi}}
+		{Literals}
+""";
+
+const string msg7 = $$""""""
+7. Raw
+	String {{pi}}
+		"""""{Literals}"""""
+"""""";
+
+const string msg8 = $$$$$$""""""
+8. Raw
+	String {{{{{{pi}}}}}}
+		"""""{{{{{Literals}}}}}"""""
+"""""";
+
+const string name = "Noah";
+const string json = $$""""
+{
+	"Name": "{{name}}"
+}
+"""";
+
+Console.WriteLine(msg1);
+Console.WriteLine(msg2);
+Console.WriteLine(msg3);
+Console.WriteLine(msg4);
+Console.WriteLine(msg5);
+Console.WriteLine(msg6);
+Console.WriteLine(msg7);
+Console.WriteLine(msg8);
+Console.WriteLine();
+Console.WriteLine(json);
---
+++ SampleExtra01.ExtendedNameofScope/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+UserEntity userEntity = new()
+{
+	Name = "Noah",
+};
+
+UserModel userModel = MapToModel(userEntity);
+Console.WriteLine(userModel);
+
+[return: NotNullIfNotNull(nameof(user))]
+static UserModel? MapToModel(UserEntity? user)
+{
+	if (user == null)
+		return null;
+
+	return new UserModel
+	{
+		Id = user.Id,
+		Name = user.Name,
+	};
+}
+
+public record UserEntity
+{
+	public long Id { get; } = Random.Shared.NextInt64();
+	
+	public required string Name { get; init; }
+}
+
+public record UserModel
+{
+	public required long Id { get; init; }
+	
+	public required string Name { get; init; }
+}
